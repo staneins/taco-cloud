77fc08e5f47a9cf554515f58f39d8a1d33f58310
Philip Kaminsky <kaminsky8836@gmail.com>
2024-12-20 15:07:37 +0300

    Revert "implement security"
    This reverts commit 93eb4e23ea0db64aa8c9884f3f2f0a9fcb2b6cf4.

--- src/test/java/com/kaminsky/tacocloud/HomeControllerTest.java
+++ src/test/java/com/kaminsky/tacocloud/HomeControllerTest.java
@@ -16,13 +16,13 @@ public class HomeControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-//    @Test
-//    public void testHomePage() throws Exception {
-//        mockMvc.perform(get("/"))
-//                .andExpect(status().isOk())
-//                .andExpect(view().name("home"))
-//                .andExpect(content().string(
-//                        containsString("Welcome to...")
-//                ));
-//    }
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content().string(
+                        containsString("Welcome to...")
+                ));
+    }
 }
